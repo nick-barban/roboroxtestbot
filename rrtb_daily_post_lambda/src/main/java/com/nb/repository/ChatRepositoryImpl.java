caa7204242be14055de0f3d2a9e9fe12b2250cb2
nickbarban <barban.mail@gmail.com>
2025-02-28 18:44:39 +0200

    [RRTB-8] return hardcoded chatId for the group RRMotivated

--- rrtb_daily_post_lambda/src/main/java/com/nb/repository/ChatRepositoryImpl.java
+++ rrtb_daily_post_lambda/src/main/java/com/nb/repository/ChatRepositoryImpl.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 public class ChatRepositoryImpl implements ChatRepository {
     @Override
     public Optional<Long> getChatIdByName(String chatName) {
-        return Optional.of(1002314637556L);
+        return Optional.of(-1002314637556L);
     }
 }
