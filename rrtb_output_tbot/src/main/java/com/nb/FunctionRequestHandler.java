888db7acce91970ed3ae8d08e9e0607044a38fa5
nickbarban <barban.mail@gmail.com>
2025-02-28 10:32:28 +0200

    feat: implement SQS event handler and add AWS Lambda Events dependency

--- rrtb_output_tbot/src/main/java/com/nb/FunctionRequestHandler.java
+++ rrtb_output_tbot/src/main/java/com/nb/FunctionRequestHandler.java
@@ -1,16 +1,23 @@
 package com.nb;
 
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.function.aws.MicronautRequestHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FunctionRequestHandler extends MicronautRequestHandler<String, String> {
+public class FunctionRequestHandler extends MicronautRequestHandler<SQSEvent, Void> {
     private static final Logger LOG = LoggerFactory.getLogger(FunctionRequestHandler.class);
 
     @Override
-    public String execute(@Nullable String input) {
-        LOG.info("Executing Lambda function with input: {}", input);
-        return "OK";
+    public Void execute(@Nullable SQSEvent event) {
+        if (event != null && event.getRecords() != null) {
+            for (SQSEvent.SQSMessage message : event.getRecords()) {
+                LOG.info("Processing SQS message with ID: {}", message.getMessageId());
+                LOG.info("Message body: {}", message.getBody());
+                // Add your message processing logic here
+            }
+        }
+        return null;
     }
 } 
