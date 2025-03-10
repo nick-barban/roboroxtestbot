8dd8f790c9c1b6883ba4f64dc5fb82bc122e8b23
nickbarban <barban.mail@gmail.com>
2025-03-10 20:14:28 +0200

    RRTB-14: Modify Main.java to use custom synthesizer that skips bootstrap validation

--- rrtb_infra/src/main/java/com/nb/Main.java
+++ rrtb_infra/src/main/java/com/nb/Main.java
@@ -3,10 +3,16 @@ package com.nb;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.DefaultStackSynthesizer;
 
 public class Main {
     public static void main(final String[] args) {
         App app = new App();
+
+        // Create a custom synthesizer that skips bootstrap validation
+        DefaultStackSynthesizer synthesizer = DefaultStackSynthesizer.Builder.create()
+            .generateBootstrapVersionRule(false)
+            .build();
 
         // Create the main application stack
         new AppStack(app, "RrtbAppStack", StackProps.builder()
@@ -14,6 +20,7 @@ public class Main {
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
                         .region(System.getenv("CDK_DEFAULT_REGION"))
                         .build())
+                .synthesizer(synthesizer)
                 .build());
 
         app.synth();
