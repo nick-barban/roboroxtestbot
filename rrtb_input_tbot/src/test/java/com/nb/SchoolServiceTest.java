7ab4d9cb78633019538367079837da696200812f
nickbarban <barban.mail@gmail.com>
2025-03-18 09:24:09 +0200

    refactor: further updates to SchoolServiceTest

--- rrtb_input_tbot/src/test/java/com/nb/SchoolServiceTest.java
+++ rrtb_input_tbot/src/test/java/com/nb/SchoolServiceTest.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchoolServiceTest extends AbstractTest {
